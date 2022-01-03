b9b9b96d80976c1944efcdbe7505506fe45e6e4f
LucasJeanpierre <61747163+LucasJeanpierre@users.noreply.github.com>
2022-01-03 15:47:33 +0100

    add non sidebar-div elements

--- sideway_navbar.js
+++ sideway_navbar.js
@@ -12,6 +12,18 @@ function closeNav() {
     document.getElementById("topheader").style.width = "100%";
     document.getElementById("headerlogo").style.opacity = "1";
     document.getElementById("headerlogo").style['pointer-events'] = "all";
+    closeAllChildren(document.getElementById('sidebar'));
+}
+
+
+function closeAllChildren(element) {
+    for (let i = 0; i < element.childElementCount; i++) {
+        var child = element.children[i];
+        if (child.classList.contains('dropdown-container')) {
+            closeAllChildren(child);
+            child.style['max-height'] = "0px";
+        } else if (child.classList.contains('active-sidebar')) child.classList.toggle("active-sidebar");
+    }
 }
 
 function switchNav() {
@@ -51,16 +63,21 @@ for (i = 0; i < dropdown.length; i++) {
         var dropdownContent = this.nextElementSibling;
         var childCount = 0
         for (var i = 0; i < dropdownContent.childElementCount; i++) {
-            if (dropdownContent.children[i].tagName != 'DIV') childCount++
+            if (!dropdownContent.children[i].classList.contains('dropdown-container')) childCount++
         }
         if ((dropdownContent.style['max-height'] === "0px") || (dropdownContent.style['max-height'] === "")) {
             //the following part is only for the dropdown animation
             dropdownContent.style['max-height'] = Math.min(43 * childCount, 250) + "px";
-            if (dropdownContent.parentElement.classList.contains('dropdown-container')) dropdownContent.parentElement.style['max-height'] = parseInt(dropdownContent.parentElement.style['max-height']) + Math.min(43 * childCount, 250) + "px";
+            element = dropdownContent;
+            while (element.parentElement.classList.contains('dropdown-container')) {
+                element.parentElement.style['max-height'] = parseInt(element.parentElement.style['max-height']) + Math.min(43 * childCount, 250) + "px";
+                element = element.parentElement;
+            }
             if (Math.min(43 * childCount, 250) < 250) {
                 dropdownContent.style['overflow-y'] = 'hidden';
             }
         } else {
+            closeAllChildren(dropdownContent);
             dropdownContent.style['max-height'] = "0px";
         }
     });
